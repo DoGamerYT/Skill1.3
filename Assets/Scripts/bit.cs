3cc25110f933bef5fafff35548f517c948d608b7
DoGamerYT <144898938+DoGamerYT@users.noreply.github.com>
2025-04-04 15:12:44 +0200

    First commit

---
+++ Assets/Scripts/bit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bit : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+   
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public bool state = false;
+   
+    void Update()
+    {
+        if (state)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        state = !state; 
+    }
+}
