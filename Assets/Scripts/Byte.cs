ecbb20ce027f5a380017796498c23d0c566c27fe
DoGamerYT <144898938+DoGamerYT@users.noreply.github.com>
2025-04-08 15:14:34 +0200

    2d

---
+++ Assets/Scripts/Byte.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Byte : MonoBehaviour
+{
+    [SerializeField] bit[] bits = new bit[8];
+    [SerializeField] private int value = 0;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Skill1.3
-yeet
+
+## **Les 1**
+hier zie je les 1 de bit
+
+[Bite](assets/Scripts/bit.cs)
+
+## **2d animation spritesheet**
+
+![2dAnimation](2dAnimation.gif)
